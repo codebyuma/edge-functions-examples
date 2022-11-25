3ea1458fe93efc95012b952122e573851bdfa812
Uma Chandran <uma.chandran@netlify.com>
2022-11-25 15:24:38 -0500

    m

--- netlify/edge-functions/environment.ts
+++ netlify/edge-functions/environment.ts
@@ -3,6 +3,8 @@ import type { Context } from "https://edge.netlify.com";
 export default async (request: Request, context: Context) => {
   const value = Deno.env.get("MY_IMPORTANT_VARIABLE");
 
+  console.log("test ", process.env);
+
   return new Response(`Value of MY_IMPORTANT_VARIABLE is "${value}".`, {
     headers: { "content-type": "text/html" },
   });
