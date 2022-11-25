e6adb98d2d6a9f73deca4638fb6aabac802fb7f7
Uma Chandran <uma.chandran@netlify.com>
2022-11-25 15:43:23 -0500

    fix

--- netlify/edge-functions/environment.ts
+++ netlify/edge-functions/environment.ts
@@ -2,6 +2,8 @@ import type { Context } from "https://edge.netlify.com";
 
 export default async (request: Request, context: Context) => {
   const value = Deno.env.get("MY_IMPORTANT_VARIABLE");
+  context.log("test");
+  context.log("deno vars context ", Deno.env.toObject());
 
   console.log("deno vars ", Deno.env.toObject());
 
